cc55c4e176dcae63c98a3a5a2d055a8bae78ef9d
Вячеслав Украинцев <slavalapin033@gmail.con>
2021-11-29 12:45:04 +0300

    Добавил ресурсы в проект(бд, изображения)

--- CinemaInformationSystem/CinemaSystemManagementApp/Program.cs
+++ CinemaInformationSystem/CinemaSystemManagementApp/Program.cs
@@ -16,7 +16,7 @@ namespace CinemaSystemManagementApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
